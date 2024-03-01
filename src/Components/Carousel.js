1645e07a208ebb77e454b000e062c5a04ed9f860
subham-del <106529527+subham-del@users.noreply.github.com>
2024-03-01 22:18:21 +0530

    my

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -4,7 +4,7 @@ function Carousel() {
 
     return (
         <div style={{ minWidth: '100%', height: '60vh', backgroundImage: 'linear-gradient(-225deg, #FF057C 0%, #8D0B93 50%, #321575 100%)', borderRadius: '10px', border: '0px' }}>
-            <div style={{ position: 'relative', width: '100%', borderRadius: '10px', border: '0px', height: '100%', backgroundImage: `url(http://localhost:3000/ecommerce/images/iphone.png)`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'right bottom' }}>
+            <div style={{ position: 'relative', width: '100%', borderRadius: '10px', border: '0px', height: '100%', backgroundImage: `url(./images/iphone.png)`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'right bottom' }}>
                 <button style={{ position: 'absolute', bottom: '10%', left: '5%', width: '13%', height: '9%', borderRadius: '5px', border: '0px', backgroundColor: 'black', color: 'white' }}>Shop now</button>
                 <div style={{ color: 'white', position: 'absolute', top: '0%', left: '5%', fontFamily: 'poppins',display:'flex',justifyContent:'center',alignItems:'center' }}>
                     <FaApple style={{ fontSize: '45px' }} />
